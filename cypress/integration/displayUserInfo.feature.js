00e922a0d67649c860a829a68200fc13a84e329d
arunbhalli <tomarbhalli@gmail.com>
2021-06-24 22:06:43 +0200

    corrections to test for displaying user information

--- cypress/integration/displayUserInfo.feature.js
+++ cypress/integration/displayUserInfo.feature.js
@@ -1,9 +1,9 @@
-describe('user can see display information',()=>{
+describe('user can see user information',()=>{
   beforeEach(()=>{
     cy.visit('/')
   })
   describe('succesfully',()=>{
-    it('is expected to show users details',()=>{
+    it('is expected to show user details',()=>{
       cy.get('[data-cy=avatar]').should('be.visible')
       cy.get('[data-cy=name]').should('contain', 'Chris moris')
       cy.get('[data-cy=location]').should('contain', 'Stockholm')
@@ -11,5 +11,4 @@ describe('user can see display information',()=>{
       cy.get('[data-cy=age]').should('contain', '25')
     })
   })
-  
 })
