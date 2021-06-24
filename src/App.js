722756f4a6a8b7c00fa45ac148251481a41909d4
arunbhalli <tomarbhalli@gmail.com>
2021-06-24 21:40:07 +0200

    modified test and user data

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 const user = {
   name: 'Chris moris',
   location: 'Stockholm',
-  foodType: 'Vagitarian',
+  foodType: 'Vegetarian',
   age: 25,
   avatar:
     'https://scotch-res.cloudinary.com/image/upload/v1556479698/xRZsnhr0_400x400_cpyg2t.png'
@@ -13,10 +13,10 @@ function App() {
   return (
     <div>
      <img data-cy='avatar' src= 'https://scotch-res.cloudinary.com/image/upload/v1556479698/xRZsnhr0_400x400_cpyg2t.png'></img>
-     <h1 data-cy='name'>{user.name}</h1>
-     <h1 data-cy='age'>{user.age}</h1>
-     <h1 data-cy='location'>{user.location}</h1>
-     <h1 data-cy='foodType'>{user.foodType}</h1>
+     <h1 data-cy='name'>User Name:{user.name}</h1>
+     <h1 data-cy='age'>Users age.{user.age}</h1>
+     <h1 data-cy='location'>Users Location:{user.location}</h1>
+     <h1 data-cy='foodType'>Food FoodType:{user.foodType}</h1>
     </div>
   );
 }
